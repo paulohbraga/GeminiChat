e005de743489bbdcabd8f12fa79a2891cd983ceb
paulohbraga <paulobragapereira@gmail.com>
2023-12-27 23:38:35 -0300

    feat: Create flutter app, add webview

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setBackgroundColor(const Color(0x00000000))
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onWebResourceError: (WebResourceError error) {},
+        ),
+      )
+      ..loadRequest(Uri.parse('https://chatbot-gemini.vercel.app/app'));
+
+    return Scaffold(
+        backgroundColor: Color.fromARGB(255, 12, 112, 145),
+        body: SafeArea(
+          child: SizedBox.expand(
+            child: Container(
+                decoration:
+                    const BoxDecoration(color: Color.fromARGB(255, 9, 51, 68)),
+                child: WebViewWidget(
+                  controller: controller,
+                )),
+          ),
+        ));
+  }
+}
